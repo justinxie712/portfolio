c883399224d3ea36403ffb5b677893f707740c62
Justin Xie <justin.xie712@gmail.com>
2025-05-09 12:10:37 -0700

    fix mobile styling

--- src/sections/Projects/index.tsx
+++ src/sections/Projects/index.tsx
@@ -7,13 +7,14 @@ const projects: Project[] = [
   {
     title: "Melodex",
     image: "/melodex.png",
-    summary: "A music data visualizer built with React and the mock data.",
+    summary:
+      "A music data visualizer prototype that allows users to search for songs and visualize their performance metrics.",
     tech: ["React", "TypeScript", "Sass"],
     demo_url: "https://justinxie712.github.io/melodex-app/",
     source_url: "https://github.com/justinxie712/melodex-app",
   },
   {
-    title: "Spreadsheet Application",
+    title: "Spreadsheet",
     image: "/spreadsheet.png",
     summary:
       "A responsive spreadsheet clone with cell formatting, formula support, and data persistence capabilities.",
@@ -25,7 +26,7 @@ const projects: Project[] = [
     title: "Tic-Tac-Toe",
     image: "/tic-tac-toe.png",
     summary:
-      "An interactive Tic-Tac-Toe game with a clean UI, move history tracking, and the ability to jump back to previous game states.",
+      "A spin on the classic game of Tic-Tac-Toe with a clean and stylish interface that is fun for all ages.",
     tech: ["React", "JavaScript", "CSS"],
     demo_url: "https://2211631.playcode.io/",
     source_url: "https://playcode.io/2211631",
